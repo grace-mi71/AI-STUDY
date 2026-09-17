fde5d585a1e53e60da49da8d63d8ad7192fb81e4
GANGMIN, LEE <jerry3708@ajou.ac.kr>
2026-09-17 12:49:23 +0900

    [Passed] 왔다 갔던 구역 2 (62ms, 16MB)

---
+++ trail2/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,29 @@
+n = int(input())
+x = []
+dir = []
+OFFSET=1000
+n_list=[0]*2001
+cnt=0
+
+for _ in range(n):
+    xi, di = input().split()
+    x.append(int(xi))
+    dir.append(di)
+
+here=OFFSET
+
+for i in range(n):
+    if dir[i]=='R':
+        for _ in range(x[i]):
+            n_list[here]+=1
+            here+=1
+    if dir[i]=='L':
+        for _ in range(x[i]):
+            here-=1
+            n_list[here]+=1
+
+for idx in range(len(n_list)):
+    if n_list[idx]>=2:
+        cnt+=1
+print(cnt)
+# Please write your code here.
